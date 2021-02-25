0d630bcda989236630e67468bb55e629e3d7a708
Julio Matheus Vaz <jmatheus.vaz@gmail.com>
2021-02-25 18:35:11 -0300

    removing strict from types base

--- lib/tdlib/types/base.rb
+++ lib/tdlib/types/base.rb
@@ -1,7 +1,7 @@
 module TD::Types
   class Base < Dry::Struct
     # throw an error when unknown keys provided
-    schema schema.strict
+    # schema schema.strict
     
     # convert string keys to symbols
     transform_keys(&:to_sym)
