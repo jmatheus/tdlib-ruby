1125e92628c790a73d78d9fd53714127a3160d78
Carolina Ransatto <cransatto@mkmservice.com>
2021-03-11 15:19:59 -0300

    autoload message senders

--- lib/tdlib/types/message_sender.rb
+++ lib/tdlib/types/message_sender.rb
@@ -1,4 +1,10 @@
 module TD::Types
   class MessageSender < Base
+    %w[
+      chat
+      user
+    ].each do |type|
+      autoload TD::Types.camelize(type), "tdlib/types/message_sender/#{type}"
+    end
   end
 end
